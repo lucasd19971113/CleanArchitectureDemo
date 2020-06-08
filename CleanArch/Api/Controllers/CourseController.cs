611da7610c3c6e139c2d4c856ae7f4f8b7d9fccc
lucasd19971113 <lucas_d_motta@outlook.com>
2020-06-08 06:56:17 -0300

    Update Controller for Api Project

---
+++ CleanArch/Api/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+        
+
+    }
+}
