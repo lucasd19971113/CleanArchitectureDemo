9392aa80963383e365e879886da0c98262ceedb6
lucasd19971113 <lucas_d_motta@outlook.com>
2020-06-08 07:44:55 -0300

    Adds CQRS to CreateCourse

--- CleanArch/Api/Controllers/CourseController.cs
+++ CleanArch/Api/Controllers/CourseController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Interfaces;
+using Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,7 +13,18 @@ namespace Api.Controllers
     [Route("api/[controller]")]
     public class CourseController : ControllerBase
     {
-        
+        private readonly ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
 
+        [HttpPost]
+        public IActionResult Create([FromBody] CourseViewModel courseViewModel)
+        {
+            _courseService.Create(courseViewModel);
+            
+            return Ok(courseViewModel);
+        }
     }
 }
--- CleanArch/Api/Startup.cs
+++ CleanArch/Api/Startup.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data.Context;
+using Infrastructure.IoC;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Api
 {
@@ -26,6 +31,23 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            AppDbContext.ConnectionString = Configuration.GetConnectionString("AppDbConnection");
+
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("AppDbConnection"));
+            });
+
+
+            services.AddSwaggerGen(options =>{
+                options.SwaggerDoc("v1", new OpenApiInfo{Title = "University API", Version = "v1"});
+            });
+
+            services.AddMediatR(typeof(Startup));
+
+
+            //Infrastructure.IoC Container Call
+            services.RegisterServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +59,12 @@ namespace Api
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>{
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "University API v1");
+            });
 
             app.UseRouting();
 
