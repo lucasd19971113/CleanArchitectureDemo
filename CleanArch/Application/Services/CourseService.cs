83db33f2b2e0ee8421123682cb8b4bbdff9eb379
lucasd19971113 <lucas_d_motta@outlook.com>
2020-06-08 06:52:45 -0300

    Adds Create Method to CourseService

--- CleanArch/Application/Services/CourseService.cs
+++ CleanArch/Application/Services/CourseService.cs
@@ -1,17 +1,33 @@
 using Application.Interfaces;
 using Application.ViewModels;
+using Domain.Commands;
+using Domain.Core.Bus;
 using Domain.Interfaces;
 
 namespace Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository _courseRepository;
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
         }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                courseViewModel.Name,
+                courseViewModel.Description,
+                courseViewModel.ImageUrl
+            );
+
+            _bus.SendCommand(createCourseCommand);
+        }
+
         public CourseViewModel GetCourses()
         {
             return new CourseViewModel()
